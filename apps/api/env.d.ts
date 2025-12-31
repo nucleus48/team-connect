255d5da113866a403cdec87236ea2ec33c8ff1fb
nucleus48 <nucleuserumagborie@gmail.com>
2025-12-31 02:40:19 +0100

    feat: Setup API application with Drizzle ORM and authentication, and streamline monorepo ESLint and Turbo configurations.

---
+++ apps/api/env.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    BETTER_AUTH_SECRET?: string;
+    BETTER_AUTH_URL?: string;
+    DB_FILE_NAME?: string;
+  }
+}
---
+++ apps/api/src/db/db.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema";
+
+const DB_FILE_NAME = process.env.DB_FILE_NAME;
+
+if (!DB_FILE_NAME) {
+  throw new Error("DB_FILE_NAME env cannot be undefined");
+}
+
+export const DbService = "DbService";
+export const db = drizzle(DB_FILE_NAME, { schema });
+
+export type DbService = typeof db;
+
+@Module({
+  providers: [
+    {
+      provide: DbService,
+      useValue: db,
+    },
+  ],
+  exports: [DbService],
+})
+export class DbModule {}
