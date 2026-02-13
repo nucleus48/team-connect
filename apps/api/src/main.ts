18404703c1a0c56b87646bdfe960fe9c3f21a8af
nucleus48 <nucleuserumagborie@gmail.com>
2026-02-13 03:18:35 +0100

    feat: Introduce Turborepo package generator and centralize shared TypeScript and Prettier configurations into dedicated packages.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,4 @@
-import dotenv from "dotenv";
-
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config();
-}
+import "dotenv/config";
 
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
